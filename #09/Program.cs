f06239905517d6dc4083bba4472c6b1458a61a76
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-02-01 16:23:04 +0200

    Problem Solution
    The solution for problem #09

--- #09/Program.cs
+++ #09/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #09
+// Given an integer x, return true if x is a palindrome, and false otherwise
+
+int num = 121;
+bool output = IsPalindrome(num);
+
+static bool IsPalindrome(int x) 
+{
+    if (ReverseInt(x) == x) {
+        return true;
+    }
+    else return false;
+}
+static int ReverseInt (int num) 
+{
+    int result = 0;
+    while (num>0) 
+    {
+        result = result*10 + num%10;
+        num /= 10;
+    }
+    return result;
+}
