993fa26ea326d4d680edd8b4da67548d40a5ca16
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-02-01 16:11:41 +0200

    Problem solution
    The solution for problem #739

--- #739/Program.cs
+++ #739/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #739
+
+int[] temperatures = {30, 60, 90};
+int[] output = DailyTemperatures(temperatures);
+
+static int[] DailyTemperatures(int[] temperatures) 
+{
+    int[] outputArr = new int[temperatures.Length]; // here we store the number of days we need to wait
+    int currTemp;
+    for (int i = 0; i < temperatures.Length; i++) {
+        currTemp = temperatures[i]; // saves the temperature that we are working with inside the currTemp variable
+        // for each current temperature, we are looking for the larger value
+        for (int j = i + 1; j < temperatures.Length; j ++) {
+            if (temperatures[j] > currTemp) { // checking if the next values are larger then our current one
+                outputArr[i] = j - i; // the difference between the index of the larger value and our current temp is the number of days we need to wait
+                break; // stops checking for greater values
+            }
+        }
+    }
+    return outputArr;
+}
+
+
