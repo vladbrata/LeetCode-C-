b4139475c8ee4e0ceea1315e6717db0b1dcf498e
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-02-01 16:18:12 +0200

    Problem solution
    The solution for problem #01

--- #01/Program.cs
+++ #01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #01
+
+int[] nums = {2, 7, 11, 15};
+int[] output = TwoSum(nums, 13);
+
+static int[] TwoSum(int[] arr, int targetNum) 
+{
+    int[] vector = new int[2];
+    // checks if the sum of each number combination is equal to targetNum
+    for (int i = 0; i < arr.Length; i++) {
+        for (int j = i + 1; j < arr.Length; j++) {
+            if (arr[i] + arr[j] == targetNum) {
+                vector[0] = i;
+                vector[1] = j;
+            }
+        }
+    }
+    return arr;
+}
