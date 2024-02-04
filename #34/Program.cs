12c7faf43ac21d4f20158ff7f85cd6b4917b71fd
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-02-04 16:21:10 +0200

    Project Soluion
    Solution for problem #34

--- #34/Program.cs
+++ #34/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #34
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+
+ static int[] SearchRange(int[] nums, int target) 
+ {
+    int[] output = {-1, -1}; // default value is unfound. the following code modifies this array if value is found
+    // the outter for loop
+    // goes trough each element of the array
+    for (int i = 0; i < nums.Length; i++) {
+        // in the first run of the outter for loop, the inner while loop iterates trough
+        // the array backwards in order, until the last position is found, so we can find
+        // both first and last positions in just one iteration
+        int j = nums.Length - 1;
+        bool found = false;
+        do {
+            if (nums[j] == target) {
+                output[1] = j;
+                found = true;
+                break;
+            }
+            j--;
+        } while (j >= 0 && !found);
+        // after the last position is found, the outter for loop continues its search for the first position
+        if (nums[i] == target) {
+            output[0] = i;
+            break;
+        }
+    }
+    return output;
+}
