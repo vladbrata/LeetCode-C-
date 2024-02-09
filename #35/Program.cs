70c34a842f0a38c845dac78c1c0e3704be16947d
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-02-09 19:49:33 +0200

    Problem solution

--- #35/Program.cs
+++ #35/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* #35
+Given a sorted array of distinct integers and a target value, 
+return the index if the target is found. If not, return the 
+index where it would be if it were inserted in order.
+*/
+int[] nums = {1,3,5,6};
+int output = SearchInsert(nums, 5);
+
+static int SearchInsert(int[] nums, int target) 
+{
+    // searches for the target num in the array
+    for(int i = 0; i < nums.Length; i++) {
+        if(nums[i] == target) {
+            return i; // if the target is found, returns the index
+        }
+    }
+    // if the target is not found, it searches for the num that is smaller than the target
+    int smallest = -1; // if no other num is smaller than the target, the target should be placed first inside the array (smallest + 1 should be 0)
+    for(int i = 0; i < nums.Length; i++) {
+        if (nums[i] < target) smallest = i; // updates the value as we iterate trough the nums
+    }
+    return smallest + 1; 
+}
