0925e99ae17aa3b07b87b6567bedf2099b230ee8
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-02-09 19:15:24 +0200

    Problem Solution
    Solution for problem #33

--- #33/Program.cs
+++ #33/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* #33
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k 
+(1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., 
+nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might 
+be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of 
+target if it is in nums, or -1 if it is not in nums.
+*/
+
+int[] nums = {4,5,6,7,0,1,2};
+int output = Search(nums, 0);
+
+// just a simple linear search algorithm
+static int Search(int[] nums, int target) {
+    for(int i = 0; i < nums.Length; i++) {
+        if (nums[i] == target) return i;
+    }
+    return -1;
+}
