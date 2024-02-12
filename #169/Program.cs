aef5d4fb8e9943b3e520dbd6a666ce6684d82b2d
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-02-12 22:23:53 +0200

    New File

--- #169/Program.cs
+++ #169/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* #169
+    Given an array nums of size n, return the majority element.
+
+    The majority element is the element that appears more than ⌊n / 2⌋ times. 
+    You may assume that the majority element always exists in the array.
+*/
+int[] nums = {3,3,4};
+int output = MajorityElement(nums);
+
+static int MajorityElement(int[] nums) 
+{
+    int contender = 0;
+    int count;
+
+    for(int i = 0; i < nums.Length; i++) {
+        count = 0;
+        contender = nums[i];
+        for(int j = 0; j < nums.Length; j++) {
+            if (contender == nums[j]) count++;
+        }
+        if (count > nums.Length /2) {
+            return contender;
+        } 
+    }
+    return contender;
+}
